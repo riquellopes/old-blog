a27012b3b92ea277795297b9762c25166838deb7
Henrique Lopes <riquellopes@gmail.com>
2016-01-10 18:57:09 -0200

    atualizando links

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,9 +21,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('PythonBrasil', 'http://wiki.python.org.br/'),)
 
 # Social widget
 SOCIAL = (('github', 'http://github.com/riquellopes'),
